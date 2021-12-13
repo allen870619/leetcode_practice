83c369423178133d772200d1068f841510b1ceb3
Allen Lee <allen870619@gmail.com>
2021-12-13 08:14:48 +0800

    1446. Consecutive Characters

---
+++ 1446. Consecutive Characters.playground/Contents.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func maxPower(_ s: String) -> Int {
+        var maxCount = 0
+        var tmp = 0
+        var tmpChar: Character?
+        for i in s{
+            if i != tmpChar{
+                maxCount = max(maxCount, tmp)
+                tmp = 1
+                tmpChar = i
+            }else{
+                tmp += 1
+            }
+        }
+        return max(maxCount, tmp)
+    }
+}
