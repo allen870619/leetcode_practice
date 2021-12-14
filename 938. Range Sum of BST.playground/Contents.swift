3a55bc6defc39fb8019bd4954ca806b59a9a2636
Allen Lee <allen870619@gmail.com>
2021-12-14 08:27:06 +0800

    938. Range Sum of BST

---
+++ 938. Range Sum of BST.playground/Contents.swift
@@ -0,0 +1,35 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        let range = low...high
+        if root.left?.val ?? low - 1 < low{
+            root.left?.left = nil
+        }
+        if root.left?.val ?? high + 1 > high{
+            root.left?.right = nil
+        }
+        if root.right?.val ?? low - 1 < low{
+            root.right?.left = nil
+        }
+        if root.right?.val ?? high + 1 > high{
+            root.right?.right = nil
+        }
+        let me = range.contains(root.val) ? root.val : 0
+        return rangeSumBST(root.left, low, high) + me + rangeSumBST(root.right, low, high)
+    }
+}
