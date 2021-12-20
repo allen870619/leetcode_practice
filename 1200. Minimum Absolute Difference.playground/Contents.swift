893a75640b859c3c8d298c88d58065980996a490
Allen Lee <allen870619@gmail.com>
2021-12-20 08:24:49 +0800

    1200. Minimum Absolute Difference

---
+++ 1200. Minimum Absolute Difference.playground/Contents.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func minimumAbsDifference(_ arr: [Int]) -> [[Int]] {
+        let arr = arr.sorted()
+        var minDis = Int.max
+        var result: [[Int]] = []
+        for i in 0..<arr.count - 1{
+            let dis = arr[i+1] - arr[i]
+            if dis < minDis{
+                result.removeAll()
+                minDis = dis
+            }
+            if dis <= minDis{
+                result.append([arr[i], arr[i+1]])
+            }
+        }
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.minimumAbsDifference([4,2,1,3]))
+print(sol.minimumAbsDifference( [1,3,6,10,15]))
+print(sol.minimumAbsDifference([3,8,-10,23,19,-4,-14,27]))
