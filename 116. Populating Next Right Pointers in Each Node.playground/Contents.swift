c4f9afac1e2543a515fc25f197de84c8a5f866ca
Allen Lee <allen870619@gmail.com>
2021-12-29 08:24:53 +0800

    116. Populating Next Right Pointers in Each Node

---
+++ 116. Populating Next Right Pointers in Each Node.playground/Contents.swift
@@ -0,0 +1,49 @@
+public class Node {
+    public var val: Int
+    public var left: Node?
+    public var right: Node?
+    public var next: Node?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+        self.next = nil
+    }
+}
+
+class Solution {
+    func connect(_ root: Node?) -> Node? {
+        guard let head = root else{
+            return nil
+        }
+        head.next = nil
+        
+        var nodeStack: [Node?] = [head]
+        while !nodeStack.isEmpty{
+            var tmp : [Node] = []
+            while !nodeStack.isEmpty{
+                if let node = nodeStack.removeFirst(){
+                    if let left = node.left{
+                        tmp.append(left)
+                    }
+                    if let right = node.right{
+                        tmp.append(right)
+                    }
+                }
+            }
+            
+            if tmp.isEmpty{
+                break
+            }
+            
+            for i in 0..<tmp.count - 1{
+                tmp[i].next = tmp[i+1]
+            }
+            tmp.last?.next = nil
+            
+            nodeStack = tmp
+            tmp.removeAll()
+        }
+        return root
+    }
+}
