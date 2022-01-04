1a31321574b473d4463882e2a81acdcf43b6b815
Allen Lee <allen870619@gmail.com>
2022-01-04 08:25:39 +0800

    1009. Complement of Base 10 Integer

---
+++ 1009. Complement of Base 10 Integer.playground/Contents.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func bitwiseComplement(_ n: Int) -> Int {
+        var twoBase = 1
+        var total = 1
+        var ori = n
+        while ori > 1{
+            ori /= 2
+            total += twoBase * 2
+            twoBase *= 2
+        }
+        return total - n
+    }
+}
+let sol = Solution()
+print(sol.bitwiseComplement(0))
+print(sol.bitwiseComplement(1))
+print(sol.bitwiseComplement(5))
+print(sol.bitwiseComplement(7))
+print(sol.bitwiseComplement(10))
