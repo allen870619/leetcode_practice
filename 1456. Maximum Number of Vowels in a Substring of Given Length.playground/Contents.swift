94b6f5914af03f21bb243e865e5880b38fb70406
Allen Lee <allen870619@gmail.com>
2022-01-21 14:18:21 +0800

    1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ 1456. Maximum Number of Vowels in a Substring of Given Length.playground/Contents.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func maxVowels(_ s: String, _ k: Int) -> Int {
+        let alpha:[Character] =  ["a", "e", "i", "o", "u"]
+        let str: [Character] = [Character](s)
+        var left = 0
+        var right = 0
+        var maxV = 0
+        var v = 0
+        while right < str.count{
+            if alpha.contains(str[right]){
+                v += 1
+            }
+            if right - left >= k{
+                if alpha.contains(str[left]){
+                    v -= 1
+                }
+                left += 1
+            }
+            maxV = max(maxV, v)
+            if maxV == k{
+                break
+            }
+            right += 1
+        }
+        return maxV
+    }
+}
+
+let sol = Solution()
+print(sol.maxVowels("abciiidef", 3))
