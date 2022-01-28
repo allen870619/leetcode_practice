9bb328437e9b71c286fc7360a118af2bd07c91fc
Allen Lee <allen870619@gmail.com>
2022-01-28 14:16:53 +0800

    211. Design Add and Search Words Data Structure

---
+++ 211. Design Add and Search Words Data Structure.playground/Contents.swift
@@ -0,0 +1,60 @@
+class WordDictionary {
+    class TireNode {
+        var count = 0
+        var value: Character?
+        var nextNode: [TireNode] = []
+        init(_ value: Character? = nil){
+            self.value = value
+        }
+    }
+    let root = TireNode()
+    
+    func addWord(_ word: String) {
+        var tmp = root
+        for i in word{
+            if let index = tmp.nextNode.firstIndex(where: {$0.value == i}){
+                tmp = tmp.nextNode[index]
+            }else{
+                let newNode = TireNode(i)
+                tmp.nextNode.append(newNode)
+                tmp = newNode
+            }
+        }
+        tmp.count += 1
+    }
+
+    var searchArr: [Character] = []
+    func search(_ word: String) -> Bool {
+        searchArr = Array(word)
+        return searchResult(root, index: 0)
+    }
+    
+    private func searchResult(_ node: TireNode, index: Int) -> Bool{
+        if index == searchArr.count{
+            return node.count > 0
+        }
+        if searchArr[index] == "."{
+            for i in node.nextNode{
+                if searchResult(i, index: index + 1){
+                    return true
+                }
+            }
+        }else{
+            if let newIndex = node.nextNode.firstIndex(where: {$0.value == searchArr[index]}){
+                return searchResult(node.nextNode[newIndex], index: index + 1)
+            }
+        }
+        return false
+    }
+}
+
+let sol = WordDictionary()
+sol.addWord("dog")
+sol.addWord("dogs")
+sol.addWord("oath")
+sol.addWord("pig")
+print(sol.search("dog"))
+print(sol.search("dig")) // f
+print(sol.search("..g"))
+print(sol.search("pigs")) // f
+print(sol.search("o..h"))
