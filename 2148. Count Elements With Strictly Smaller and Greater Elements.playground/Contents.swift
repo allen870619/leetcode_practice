87442fba7a7fa8324c55badc6aa87003726ab42e
Allen Lee <allen870619@gmail.com>
2022-01-30 12:14:59 +0800

    2148. Count Elements With Strictly Smaller and Greater Elements

---
+++ 2148. Count Elements With Strictly Smaller and Greater Elements.playground/Contents.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func countElements(_ nums: [Int]) -> Int {
+        var nums = nums.sorted()
+        var min = nums[0]
+        var max = nums.last!
+        var count = 0
+        for i in nums{
+            if i > min && i < max{
+                count += 1
+            }
+        }
+        return count
+    }
+}
