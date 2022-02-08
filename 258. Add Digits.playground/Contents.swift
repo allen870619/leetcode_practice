2075fc0618ce806ff2791a8507387ed5522540c3
Allen Lee <allen870619@gmail.com>
2022-02-08 08:14:34 +0800

    258.Add Digits (second)

--- 258. Add Digits.playground/Contents.swift
+++ 258. Add Digits.playground/Contents.swift
@@ -1,4 +1,33 @@
+// SECOND
+class Solution3 {
+    func addDigits(_ num: Int) -> Int {
+        var num = num
+        while num >= 10{
+            var result = 0
+            while num > 0 {
+                result += num % 10
+                num /= 10
+            }
+            num = result
+        }
+        return num
+    }
+}
 
+//tricky math
+class Solution4 {
+    func addDigits(_ num: Int) -> Int {
+        if num < 10{
+            return num
+        }else if num % 9 == 0{
+            return 9
+        }else{
+            return num % 9
+        }
+    }
+}
+
+// FIRST
 class Solution {
     func addDigits(_ num: Int) -> Int {
         var total = num
