6b0e283c075b18838f5ef9ce695ec911beeb9a18
Allen Lee <allen870619@gmail.com>
2022-03-01 08:50:06 +0800

    228. Summary Ranges 338. Counting Bits

---
+++ 228. Summary Ranges.playground/Contents.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        if nums.count < 1{
+            return []
+        }
+        var result = [String]()
+        var tmpStart = nums[0]
+        var cur = nums[0]
+        let nums = nums + [Int.max]
+        
+        for i in nums[1...]{
+            if i == cur + 1{
+                cur += 1
+            }else{
+                if cur - tmpStart == 0{
+                    result.append("\(cur)")
+                }else{
+                    result.append("\(tmpStart)->\(cur)")
+                }
+                tmpStart = i
+                cur = i
+            }
+        }
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.summaryRanges([0,1,2,4,5,7]))
+print(sol.summaryRanges([0,2,3,4,6,8,9]))
+print(Int.max)
+print(Int32.max)
---
+++ 338. Counting Bits.playground/Contents.swift
@@ -0,0 +1,60 @@
+class Solution {
+    func countBits(_ n: Int) -> [Int] {
+        var result = [Int]()
+        for i in 0...n{
+            result.append(cal(i))
+        }
+        return result
+    }
+    
+    private func cal(_ num: Int) -> Int{
+        var total = 0
+        var cal = num
+        while cal > 0{
+            total += cal % 2
+            cal /= 2
+        }
+        return  total
+    }
+}
+
+class Solution2 {
+    func countBits(_ n: Int) -> [Int] {
+        var cMax = 1
+        var index = 0
+        var result = Array(repeating: 0, count: n + 1)
+        if n == 0{
+            return result
+        }
+        for i in 1...n{
+            result[i] = result[index] + 1
+            if result[index] + 1 >= cMax{
+                cMax += 1
+                index = 0
+            }else{
+                index += 1
+            }
+        }
+        return result
+    }
+}
+
+class Solution3 {
+    func countBits(_ n: Int) -> [Int] {
+        var result = Array.init(repeating: 0, count: n + 1)
+        if n == 0{
+            return result
+        }
+        for i in 1...n{
+            result[i] = result[i & (i - 1)] + 1
+        }
+        return result
+    }
+}
+
+let sol = Solution2()
+print(sol.countBits(0))
+print(sol.countBits(1))
+print(sol.countBits(2))
+print(sol.countBits(5))
+print(sol.countBits(20))
