28e5146e2059e48029adb42d1d94bcd1d1ec5bf1
Allen Lee <allen870619@gmail.com>
2022-03-02 08:33:08 +0800

    392. Is Subsequence

---
+++ 392. Is Subsequence.playground/Contents.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        if s.isEmpty{
+            return true
+        }
+        if t.isEmpty{
+            return false
+        }
+        
+        let sub = [Character](s)
+        var index = 0
+        for tc in t{
+            if tc == sub[index]{
+                index += 1
+                if index == sub.count{
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
+let sol = Solution()
+print(sol.isSubsequence("", "ahbgdc"))
+print(sol.isSubsequence("abc", ""))
+print(sol.isSubsequence("abc", "ahbgdc"))
+print(sol.isSubsequence("axc", "ahbgdc"))
