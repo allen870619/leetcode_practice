ca4bcb5011a481daa09881376924275e8f3549e9
Allen Lee <allen870619@gmail.com>
2022-03-04 08:45:24 +0800

    799. Champagne Tower

---
+++ 799. Champagne Tower.playground/Contents.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func champagneTower(_ poured: Int, _ query_row: Int, _ query_glass: Int) -> Double {
+        var cup = Array(repeating: Array(repeating: 0.0, count: 101), count: 101)
+        cup[0][0] = Double(poured)
+        if query_row == 0{
+            return Double(min(1, poured))
+        }
+        for i in 1...query_row{
+            for j in 0...query_glass{
+                let remain = (cup[i-1][j] - 1) / 2
+                if remain > 0{
+                    cup[i][j] += remain
+                    cup[i][j+1] += remain
+                }
+            }
+        }
+        return min(cup[query_row][query_glass], 1)
+    }
+}
+
+let sol = Solution()
+print(sol.champagneTower(0, 0, 0))
+print(sol.champagneTower(2, 1, 1))
+print(sol.champagneTower(100000009, 33, 17))
