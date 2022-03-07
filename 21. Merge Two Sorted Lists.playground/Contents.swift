878329fdf8799a543f41e95c5b7d7b662cfd0659
Allen Lee <allen870619@gmail.com>
2022-03-07 08:09:26 +0800

    21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists.playground/Contents.swift
@@ -0,0 +1,35 @@
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        var left = list1
+        var right = list2
+        var resultHead = ListNode()
+        var tmp: ListNode? = resultHead
+        while left != nil || right != nil{
+            if left != nil && right != nil{
+                if left!.val > right!.val{
+                    tmp?.next = left
+                    left = left?.next
+                }else{
+                    tmp?.next = right
+                    right = right?.next
+                }
+            }else if left != nil{
+                tmp?.next = left
+                left = left?.next
+            }else{
+                tmp?.next = right
+                right = right?.next
+            }
+            tmp = tmp?.next
+        }
+        return  resultHead.next
+    }
+}
