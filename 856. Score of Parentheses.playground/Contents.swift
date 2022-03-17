e85bc392a93a091d86f67f3c6e4bbaa9ea2dabb2
Allen Lee <allen870619@gmail.com>
2022-03-17 09:06:04 +0800

    856. Score of Parentheses

---
+++ 856. Score of Parentheses.playground/Contents.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func scoreOfParentheses(_ s: String) -> Int {
+        var level = 0
+        var total = 0
+        var lastC: Character?
+        for i in s{
+            if i == "("{
+                level += 1
+            }else{
+                level -= 1
+                if lastC == "("{
+                    total += 1 << level
+                }
+            }
+            lastC = i
+        }
+        return total
+    }
+}
+
+let sol = Solution()
+print(sol.scoreOfParentheses("()(()())"))
+print(sol.scoreOfParentheses("(()(()))"))
