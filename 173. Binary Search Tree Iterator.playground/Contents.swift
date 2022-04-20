33b27ddcc849fec8e309b47c3e93f094c55bb286
Allen Lee <allen870619@gmail.com>
2022-04-20 08:15:36 +0800

    173. Binary Search Tree Iterator

---
+++ 173. Binary Search Tree Iterator.playground/Contents.swift
@@ -0,0 +1,45 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class BSTIterator {
+    var node: TreeNode?
+    
+    init(_ root: TreeNode?) {
+        self.node = root
+    }
+    
+    func next() -> Int {
+        var tmp = node
+        if tmp?.left == nil{
+            let result = tmp?.val ?? 0
+            node = node?.right
+            return result
+        }
+        var last: TreeNode?
+        while tmp?.left != nil{
+            last = tmp
+            tmp = tmp?.left
+        }
+        let result = tmp?.val ?? 0
+        if tmp?.right != nil{
+            last?.left = tmp?.right
+        }else{
+            last?.left = nil
+        }
+        return result
+    }
+    
+    func hasNext() -> Bool {
+        return node != nil
+    }
+}
