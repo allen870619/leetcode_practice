1f469a28975422cda4d2a92898deb245a28c7b1e
Allen Lee <allen870619@gmail.com>
2022-04-21 08:10:44 +0800

    705. Design HashSet

---
+++ 705. Design HashSet.playground/Contents.swift
@@ -0,0 +1,18 @@
+class MyHashSet {
+    var array = Set<Int>()
+    init() {
+        array.removeAll()
+    }
+    
+    func add(_ key: Int) {
+        array.insert(key)
+    }
+    
+    func remove(_ key: Int) {
+        array.remove(key)
+    }
+    
+    func contains(_ key: Int) -> Bool {
+        return array.contains(key)
+    }
+}
