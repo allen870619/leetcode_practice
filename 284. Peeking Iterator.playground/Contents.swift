9e9a359c9b5c8f6a70e4d392d5081a592bde3c46
Allen Lee <allen870619@gmail.com>
2022-04-25 08:25:00 +0800

    284. Peeking Iterator

---
+++ 284. Peeking Iterator.playground/Contents.swift
@@ -0,0 +1,58 @@
+class PeekingIterator { // Array
+    var a = [Int]()
+    var pointer = -1
+    init(_ arr: IndexingIterator<Array<Int>>) {
+        for i in arr.enumerated(){
+            a.append(i.element)
+        }
+    }
+    
+    func next() -> Int {
+        pointer += 1
+        return a[pointer]
+    }
+    
+    func peek() -> Int {
+        return a[pointer + 1]
+    }
+    
+    func hasNext() -> Bool {
+        pointer != a.count - 1
+    }
+}
+
+
+class PeekingIterator2 { // Node
+    class Node{
+        var val: Int
+        var next: Node?
+        init(_ val: Int){
+            self.val = val
+        }
+    }
+    
+    var head: Node? = Node(-1)
+    
+    init(_ arr: IndexingIterator<Array<Int>>) {
+        var tmp = head
+        for i in arr.enumerated(){
+            head?.next = Node(i.element)
+            head = head?.next
+        }
+        head = tmp
+    }
+    
+    func next() -> Int {
+        let t = head?.next?.val ?? -1
+        head = head?.next
+        return t
+    }
+    
+    func peek() -> Int {
+        return head?.next?.val ?? -1
+    }
+    
+    func hasNext() -> Bool {
+        return head?.next != nil
+    }
+}
