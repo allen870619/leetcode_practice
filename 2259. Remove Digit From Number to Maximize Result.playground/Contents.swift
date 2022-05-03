4ffa9f1ebd6e6cedf26b6ebf02156f8f371b6175
Allen Lee <allen870619@gmail.com>
2022-05-03 17:19:31 +0800

    2260. Minimum Consecutive Cards to Pick Up 2259. Remove Digit From Number to Maximize Result

---
+++ 2259. Remove Digit From Number to Maximize Result.playground/Contents.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func removeDigit(_ number: String, _ digit: Character) -> String {
+        var number = [Character](number)
+        var ansList = [Int]()
+        var lastTarget = 0
+        for i in 0..<number.count{
+            if number[i] == digit{
+                if i + 1 < number.count{
+                    if number[i+1] > digit{
+                        number.remove(at: i)
+                        lastTarget = -1
+                        break
+                    }else{
+                        lastTarget = i
+                    }
+                }else{
+                    lastTarget = i
+                }
+            }
+        }
+        
+        if lastTarget != -1{
+            number.remove(at: lastTarget)
+        }
+        
+        return String(number)
+    }
+}
+
+let sol = Solution()
+print(sol.removeDigit("123", "3"))
+print(sol.removeDigit("123", "1"))
+print(sol.removeDigit("1231", "1"))
+print(sol.removeDigit("551", "5"))
+print(sol.removeDigit("133235", "3"))
