fdb11dd423bbc84b05590516b87604e306a05a4b
Allen Lee <allen870619@gmail.com>
2022-05-08 14:52:40 +0800

    341. Flatten Nested List Iterator 2264. Largest 3-Same-Digit Number in String 2265. Count Nodes Equal to Average of Subtree 2266. Count Number of Texts

---
+++ 2264. Largest 3-Same-Digit Number in String.playground/Contents.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func largestGoodInteger(_ num: String) -> String {
+        var list = [String]()
+        var last = Character(" ")
+        var count = 0
+        for i in num{
+            if i == last{
+                count += 1
+                if count == 3{
+                    list.append("\(i)\(i)\(i)")
+                }
+            }else{
+                last = i
+                count = 1
+            }
+        }
+        list.sort(by: { Int($0)! < Int($1)! })
+        
+        return list.last != nil ? list.last! : ""
+    }
+}
+
+let sol = Solution()
+print(sol.largestGoodInteger("6777133339"))
+print(sol.largestGoodInteger("2300019"))
+print(sol.largestGoodInteger("42352338"))
---
+++ 2265. Count Nodes Equal to Average of Subtree.playground/Contents.swift
@@ -0,0 +1,51 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func averageOfSubtree(_ root: TreeNode?) -> Int {
+        guard let root = root else{
+            return 0
+        }
+        
+        func calTotal(root: TreeNode) -> (count: Int, sum: Int) {
+            if root.left == nil && root.right == nil{
+                total += 1
+                return (1, root.val)
+            }
+            
+            var l = (count: 0, sum: 0); var r = (count: 0, sum: 0)
+            if let left = root.left{
+                l = calTotal(root: left)
+            }
+            if let right = root.right{
+                r = calTotal(root: right)
+            }
+            let t = (count: l.count + r.count + 1, sum: l.sum + r.sum + root.val)
+            if t.sum / t.count == root.val{
+                total += 1
+            }
+            return t
+        }
+        
+        var total = 0
+        calTotal(root: root)
+        return total
+    }
+}
+
+let sol = Solution()
+let tree = TreeNode(4, TreeNode(8, TreeNode(0), TreeNode(1)), TreeNode(5, nil, TreeNode(6)))
+
+print(sol.averageOfSubtree(tree))
+print(sol.averageOfSubtree(TreeNode(1)))
+print(sol.averageOfSubtree(TreeNode(0, TreeNode(0),TreeNode(0))))
