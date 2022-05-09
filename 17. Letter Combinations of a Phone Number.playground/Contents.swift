2c676a7e93df6b83716f4a5ce4c2720c2cc92776
Allen Lee <allen870619@gmail.com>
2022-05-09 08:22:59 +0800

    17. Letter Combinations of a Phone Number (rewrite)

--- 17. Letter Combinations of a Phone Number.playground/Contents.swift
+++ 17. Letter Combinations of a Phone Number.playground/Contents.swift
@@ -1,3 +1,29 @@
+// 202200509 second
+class Solution2 {
+    func letterCombinations(_ digits: String) -> [String] {
+        var result = [String]()
+        let keyPair = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        
+        for i in digits{
+            let index = Int("\(i)")! - 2
+            if result.isEmpty{
+                for j in keyPair[index]{
+                    result.append("\(j)")
+                }
+            }else{
+                for j in result{
+                    let lastSet = result.removeFirst()
+                    for k in keyPair[index]{
+                        result.append("\(lastSet)\(k)")
+                    }
+                }
+            }
+        }
+        return result
+    }
+}
+
+//
 class Solution {
     func letterCombinations(_ digits: String) -> [String] {
         let keyboard: [Character: String] = ["2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"]
