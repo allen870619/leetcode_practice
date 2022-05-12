061f4922f9b54b57a28d943ddbd8fb1a45edaa62
Allen Lee <allen870619@gmail.com>
2022-05-12 14:12:38 +0800

    47. Permutations II

---
+++ 47. Permutations II.playground/Contents.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func permuteUnique(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        var set = Set<[Int]>()
+        func addData(remain: [Int], tmp: [Int]){
+            if set.contains(tmp){
+                return
+            }
+            set.insert(tmp)
+            if remain.count == 0{
+                result.append(tmp)
+                return
+            }
+            for (index, i) in remain.enumerated(){
+                var r = remain
+                r.remove(at: index)
+                addData(remain: r, tmp: tmp + [i])
+            }
+        }
+        addData(remain: nums, tmp: [])
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.permuteUnique([1,2,3,4]))
+print(sol.permuteUnique([1,1,1,1,1,1,2]))
+print(sol.permuteUnique([1,2,3,4,5]))
