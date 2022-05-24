2d7b0ef100da53dfefeb2bf96ef6d173fc5a0bab
Allen Lee <allen870619@gmail.com>
2022-05-24 08:56:41 +0800

    32. Longest Valid Parentheses

---
+++ 32. Longest Valid Parentheses.playground/Contents.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func longestValidParentheses(_ s: String) -> Int {
+        let s = [Character](s)
+        if s.count == 0{
+            return 0
+        }
+        var result = 0
+        var dp = Array(repeating: 0, count: s.count)
+        
+        for (i, c) in s.enumerated(){
+            if i > 0{
+                if c == ")"{
+                    let shift = i-dp[i-1]-1
+                    if s[i-1] == "("{
+                        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2
+                    }else if shift >= 0 && s[shift] == "("{
+                        dp[i] = dp[i-1] + (shift - 1 >= 0 ? dp[shift-1] : 0) + 2
+                    }
+                    result = max(result, dp[i])
+                }
+            }
+        }
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.longestValidParentheses("())((())"))
+print(sol.longestValidParentheses(")()())"))
+print(sol.longestValidParentheses("(()"))
+print(sol.longestValidParentheses(""))
+print(sol.longestValidParentheses("()(()"))
+print(sol.longestValidParentheses("()(())"))
+print(sol.longestValidParentheses("(()())"))
+print(sol.longestValidParentheses(")(((((()())()()))()(()))("))
