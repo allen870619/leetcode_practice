5214adbde54ce861992d1563b4fb9c0a0aed563a
Allen Lee <allen870619@gmail.com>
2022-05-27 08:03:19 +0800

    1342. Number of Steps to Reduce a Number to Zero

---
+++ 1342. Number of Steps to Reduce a Number to Zero.playground/Contents.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func numberOfSteps(_ num: Int) -> Int {
+        var num = num
+        var total = 0
+        while num > 0{
+            if num % 2 == 0{
+                num /= 2
+            }else{
+                num -= 1
+            }
+            total += 1
+        }
+        return total
+    }
+}
