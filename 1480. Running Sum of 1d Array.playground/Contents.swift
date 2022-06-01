940e0bda4921309b288e79b1fb4665f0b3db64fd
Allen Lee <allen870619@gmail.com>
2022-06-01 08:09:20 +0800

    1480. Running Sum of 1d Array

---
+++ 1480. Running Sum of 1d Array.playground/Contents.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        if nums.count <= 1{
+            return nums
+        }
+        
+        for i in 1..<nums.count{
+            nums[i] = nums[i] + nums[i-1]
+        }
+        return nums
+    }
+}
+
+let sol = Solution()
+print(sol.runningSum([3,1,2,10,1]))
