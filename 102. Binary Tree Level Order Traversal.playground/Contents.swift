329b3d2e231654b307117e58a4a0ec2ab0770124
Allen Lee <allen870619@gmail.com>
2022-07-13 09:39:20 +0800

    102. Binary Tree Level Order Traversal

---
+++ 102. Binary Tree Level Order Traversal.playground/Contents.swift
@@ -0,0 +1,37 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else{
+            return []
+        }
+        var result = [[Int]]()
+        var list = [root]
+        while !list.isEmpty{
+            var tmp = [Int]()
+            for i in list{
+                let target = list.removeFirst()
+                tmp.append(target.val)
+                if let left = target.left{
+                    list.append(left)
+                }
+                if let right = target.right{
+                    list.append(right)
+                }
+            }
+            result.append(tmp)
+        }
+        return result
+    }
+}
