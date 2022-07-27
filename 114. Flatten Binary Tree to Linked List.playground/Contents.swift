0c752190170515f8e6f883677756f9c62c095d21
Allen Lee <allen870619@gmail.com>
2022-07-27 08:42:24 +0800

    114. Flatten Binary Tree to Linked List

---
+++ 114. Flatten Binary Tree to Linked List.playground/Contents.swift
@@ -0,0 +1,34 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func flatten(_ root: TreeNode?) {
+        var list = [TreeNode?]()
+        func flatIt(_ node: TreeNode?){
+            guard let node = node else{
+                return
+            }
+            list.append(node)
+            flatIt(node.left)
+            flatIt(node.right)
+        }
+        
+        flatIt(root)
+        var head = list.first!
+        for i in list[1...]{
+            head?.left = nil
+            head?.right = i
+            head = head?.right
+        }
+    }
+}
