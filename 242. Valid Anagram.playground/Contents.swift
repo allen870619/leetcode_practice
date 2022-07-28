a2e3d2b03ad218f6a9578b28633599683c0d9668
Allen Lee <allen870619@gmail.com>
2022-07-28 08:17:33 +0800

    242. Valid Anagram

---
+++ 242. Valid Anagram.playground/Contents.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        if s.count != t.count{
+            return false
+        }
+        
+        var dict1 = [Character: Int]()
+        var dict2 = [Character: Int]()
+        
+        for i in s{
+            dict1[i, default:0] += 1
+        }
+        for i in t{
+            dict2[i, default:0] += 1
+        }
+        return dict1 == dict2
+    }
+}
+class Solution2 {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        return s.sorted() == t.sorted()
+    }
+}
+
