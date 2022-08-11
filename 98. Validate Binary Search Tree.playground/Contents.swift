706f224935ec90c15e2a8a76633c70f5821ebca6
Allen Lee <allen870619@gmail.com>
2022-08-11 08:27:24 +0800

    98. Validate Binary Search Tree

---
+++ 98. Validate Binary Search Tree.playground/Contents.swift
@@ -0,0 +1,42 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        return calBST(root, top: nil, bot: nil)
+    }
+    
+    private func calBST(_ node: TreeNode?, top: Int?, bot: Int?) -> Bool{
+        guard let node = node else {
+            return true
+        }
+        // self
+        if let top = top {
+            if top <= node.val{
+                return false
+            }
+        }
+        if let bot = bot {
+            if bot >= node.val{
+                return false
+            }
+        }
+        
+        // leaf
+        return calBST(node.left, top: node.val, bot: bot) && calBST(node.right, top: top, bot: node.val)
+    }
+}
+
+let node = TreeNode(2, TreeNode(2), TreeNode(2))
+let sol = Solution()
+print(sol.isValidBST(node))
