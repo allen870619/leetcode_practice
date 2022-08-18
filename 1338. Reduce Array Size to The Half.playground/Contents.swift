6d47d55d4f8ac30f13009f7a82ceef1b0e0689d1
Allen Lee <allen870619@gmail.com>
2022-08-18 08:28:25 +0800

    1338. Reduce Array Size to The Half

---
+++ 1338. Reduce Array Size to The Half.playground/Contents.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func minSetSize(_ arr: [Int]) -> Int {
+        var countDict = [Int: Int]()
+        for i in arr {
+            countDict[i, default: 0] += 1
+        }
+        
+        var timeDict = [Int: Int]()
+        for i in countDict.values {
+            timeDict[i, default: 0] += 1
+        }
+        
+        var tmp = 0
+        var result = 0
+        var keys = timeDict.keys.sorted()
+        var last = keys.popLast()
+        while tmp * 2 < arr.count {
+            if let time = last {
+                if timeDict[time]! > 0 {
+                    tmp += time
+                    timeDict[time]! -= 1
+                    result += 1
+                } else {
+                    last = keys.popLast()!
+                }
+            }
+        }
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.minSetSize([3,3,3,3,5,5,5,2,2,7]))
+print(sol.minSetSize([7,7,7,7,7,7]))
