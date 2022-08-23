30f1e93168cb294691be498d6578430816e4f68a
Allen Lee <allen870619@gmail.com>
2022-08-23 08:30:32 +0800

    234. Palindrome Linked List

---
+++ 234. Palindrome Linked List.playground/Contents.swift
@@ -0,0 +1,30 @@
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func isPalindrome(_ head: ListNode?) -> Bool {
+        var node = head
+        var list = [ListNode]()
+        while head != nil {
+            if let node = node {
+                list.append(node)
+            }
+            node = node?.next
+        }
+        
+        var left = 0 , right = list.count - 1
+        while left <= right {
+            if list[left].val != list[right].val {
+                return false
+            }
+            left += 1
+            right -= 1
+        }
+        return true
+    }
+}
