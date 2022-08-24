89c3fc0fcea20fd1d6b13aed4bf522e8740d29f4
Allen Lee <allen870619@gmail.com>
2022-08-24 09:25:22 +0800

    326. Power of Three

---
+++ 326. Power of Three.playground/Contents.swift
@@ -0,0 +1,45 @@
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        if n == 1 {
+            return true
+        }
+        if n % 3 != 0 || n <= 0 {
+            return false
+        }
+        return isPowerOfThree(n / 3)
+    }
+}
+
+class Solution2 {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        // 1162261467: The num cloest to Int.max(), 3^19
+        return n > 0 && 1162261467 % n == 0
+    }
+}
+
+class Solution3 {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        var start: Int64 = 1
+        let n = Int64(n)
+        while start <= n {
+            if start == n {
+                return true
+            }
+            start *= 3
+        }
+        return false
+    }
+}
+
+class Solution4 {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        if n <= 0 {
+            return false
+        }
+        var n = n
+        while n % 3 == 0 {
+            n /= 3
+        }
+        return n == 1
+    }
+}
