58737d8e23acf581b7e216c31307534f491e266f
Allen Lee <allen870619@gmail.com>
2022-09-01 08:22:15 +0800

    1448. Count Good Nodes in Binary Tree

---
+++ 1448. Count Good Nodes in Binary Tree.playground/Contents.swift
@@ -0,0 +1,35 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func goodNodes(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        var result = 0
+        
+        func calResult(_ node: TreeNode?, _ maxValue: Int) {
+            guard let node = node else {
+                return
+            }
+            if node.val >= maxValue {
+                result += 1
+            }
+            let curMax = max(maxValue, node.val)
+            calResult(node.left, curMax)
+            calResult(node.right, curMax)
+        }
+        calResult(root, root.val)
+        return result
+    }
+}
