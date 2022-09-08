26c9bf8f8a2947ec67fa38e19f6050e542b85d01
Allen Lee <allen870619@gmail.com>
2022-09-08 08:30:24 +0800

    100. Same Tree

---
+++ 100. Same Tree.playground/Contents.swift
@@ -0,0 +1,51 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution { // DFS
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if (p != nil && q == nil) || (p == nil && q != nil) {
+            return false
+        }
+        if let p = p, let q = q {
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+        }
+        return true
+    }
+}
+
+class Solution2 { // BFS
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        var pList = [p]
+        var qList = [q]
+        while !pList.isEmpty && !qList.isEmpty {
+            if pList.count != qList.count {
+                return false
+            }
+            for i in 0..<pList.count {
+                let pf = pList.removeFirst()
+                let qf = qList.removeFirst()
+                if (pf != nil && qf == nil) || (pf == nil && qf != nil) || (pf?.val != qf?.val) {
+                    return false
+                }
+                if !(pf == nil && qf == nil) {
+                    pList.append(pf?.left)
+                    pList.append(pf?.right)
+                    qList.append(qf?.left)
+                    qList.append(qf?.right)
+                }
+            }
+        }
+        return true
+    }
+}
+
