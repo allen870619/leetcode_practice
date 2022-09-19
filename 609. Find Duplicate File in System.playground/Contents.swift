086606d53d8db32e0a8669632a056b7a46588a0c
Allen Lee <allen870619@gmail.com>
2022-09-19 09:16:30 +0800

    609. Find Duplicate File in System

---
+++ 609. Find Duplicate File in System.playground/Contents.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func findDuplicate(_ paths: [String]) -> [[String]] {
+        var dict = [String: [String]]()
+        
+        for i in paths {
+            let list = i.split(separator: " ")
+            let path = "\(list[0])"
+            for rawName in list[1...] {
+                let name = [Character](rawName)
+                if let start = name.firstIndex(of: "(") {
+                    let fileName = String(name[start+1..<name.count-1])
+                    dict[fileName, default: []].append("\(path)/\(String(name[0..<start]))")
+                }
+            }
+        }
+        var result = [[String]]()
+        for i in dict {
+            if i.value.count > 1 {
+                result.append(i.value)
+            }
+        }
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.findDuplicate(["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"]))
