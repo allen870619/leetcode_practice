53068a504903ccaf40bbb8a9d4f42ab683450a2a
Allen Lee <allen870619@gmail.com>
2022-10-18 08:27:50 +0800

    38. Count and Say

---
+++ 38. Count and Say.playground/Contents.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func countAndSay(_ n: Int) -> String {
+        if n == 1 {
+            return "1"
+        }
+        var result = ""
+        var value = Character("a"), count = 0
+        
+        for i in countAndSay(n-1) {
+            if i == value {
+                count += 1
+            } else {
+                if count != 0 {
+                    result += "\(count)\(value)"
+                }
+                value = i
+                count = 1
+            }
+        }
+        if count != 0 {
+            result += "\(count)\(value)"
+        }
+        return result
+    }
+}
+
+let sol = Solution()
+print(sol.countAndSay(1))
+print(sol.countAndSay(4))
+print(sol.countAndSay(5))
