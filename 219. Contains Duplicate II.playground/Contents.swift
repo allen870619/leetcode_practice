a26213ca65965e6bb6a73f67fdf9c4238cc591e5
Allen Lee <allen870619@gmail.com>
2022-10-21 08:37:40 +0800

    219. Contains Duplicate II

---
+++ 219. Contains Duplicate II.playground/Contents.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var dict = [Int: [Int]]()
+        for (index, i) in nums.enumerated() {
+            dict[i, default: []].append(index)
+        }
+        
+        for i in dict {
+            let list = i.value
+            if list.count >= 2 {
+                var last = list[0]
+                for j in list[1...] {
+                    if j - last == k {
+                        return true
+                    }
+                    last = j
+                }
+            }
+        }
+        return false
+    }
+}
