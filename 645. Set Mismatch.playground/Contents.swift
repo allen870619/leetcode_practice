78136293ad85f8348e50af116792d4870517e29d
Allen Lee <allen870619@gmail.com>
2022-10-23 11:50:18 +0800

    645. Set Mismatch

---
+++ 645. Set Mismatch.playground/Contents.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func findErrorNums(_ nums: [Int]) -> [Int] {
+        var dict = [Int: Int]()
+        var dup = 0
+        for i in nums {
+            dict[i, default: 0] += 1
+            if dict[i] ?? 0 > 1 {
+                dup = i
+            }
+        }
+        
+        for i in 1...nums.count {
+            if dict[i] == nil {
+                return [dup, i]
+            }
+        }
+        return []
+    }
+}
