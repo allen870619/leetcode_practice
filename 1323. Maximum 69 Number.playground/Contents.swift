56dc171c641b79789f407ff2848969d9b7c63158
Allen Lee <allen870619@gmail.com>
2022-11-07 08:17:31 +0800

    1323. Maximum 69 Number

--- 1323. Maximum 69 Number.playground/Contents.swift
+++ 1323. Maximum 69 Number.playground/Contents.swift
@@ -1,3 +1,12 @@
-import UIKit
-
-var greeting = "Hello, playground"
+class Solution {
+    func maximum69Number(_ num: Int) -> Int {
+        var char = [Character]("\(num)")
+        for i in 0 ..< char.count {
+            if char[i] == "6" {
+                char[i] = "9"
+                break
+            }
+        }
+        return Int(String(char)) ?? num
+    }
+}
