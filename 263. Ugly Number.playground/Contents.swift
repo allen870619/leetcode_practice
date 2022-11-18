4f033cab64853075ad9e65b793285ba60c45988f
Allen Lee <allen870619@gmail.com>
2022-11-18 08:23:41 +0800

    263. Ugly Number

---
+++ 263. Ugly Number.playground/Contents.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func isUgly(_ n: Int) -> Bool {
+        if n < 1 {
+            return false
+        }
+        var n = n
+        while n > 1 {
+            if n % 5 == 0 {
+                n /= 5
+            } else if n % 3 == 0 {
+                n /= 3
+            } else if n % 2 == 0 {
+                n /= 2
+            } else {
+                return  false
+            }
+        }
+        return true
+    }
+}
