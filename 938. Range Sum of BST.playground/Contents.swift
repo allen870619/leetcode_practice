871c2fdc0d0e2a8b71bf0f00d65854ee582580e9
Allen Lee <allen870619@gmail.com>
2022-12-07 08:40:51 +0800

    938. Range Sum of BST

--- 938. Range Sum of BST.playground/Contents.swift
+++ 938. Range Sum of BST.playground/Contents.swift
@@ -11,6 +11,36 @@ public class TreeNode {
     }
 }
 
+// Second: 20221207
+class Solution2 { // DFS
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        guard let root else { return 0 }
+        let center = (low ... high).contains(root.val) ? root.val : 0
+        return rangeSumBST(root.left, low, high) + center + rangeSumBST(root.right, low, high)
+    }
+}
+
+class Solution3 { // BFS
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        var list = [root]
+        var result = 0
+        while !list.isEmpty {
+            for _ in list {
+                let node = list.removeFirst()
+                if let node {
+                    if (low ... high).contains(node.val) {
+                        result += node.val
+                    }
+                    list.append(node.right)
+                    list.append(node.left)
+                }
+            }
+        }
+        return result
+    }
+}
+
+// First: Unknown
 class Solution {
     func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
         guard let root else {
