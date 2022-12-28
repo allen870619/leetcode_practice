f2d8095c8cd7f3500695efd67059b96e9a116898
Allen Lee <allen870619@gmail.com>
2022-12-28 17:28:28 +0800

    Update solution for 1962

--- 1501-2000/1962. Remove Stones to Minimize the Total.playground/Contents.swift
+++ 1501-2000/1962. Remove Stones to Minimize the Total.playground/Contents.swift
@@ -1,262 +1,98 @@
-// ref here: https://github.com/kodecocodes/swift-algorithm-club
 class Solution {
     func minStoneSum(_ piles: [Int], _ k: Int) -> Int {
         var piles = piles
         var total = piles.reduce(0, +)
-        var h = PriorityQueue<Int>.init { $0 > $1 }
+        var h = Heap<Int>(comparison: >)
         for i in piles {
-            h.enqueue(i)
+            h.enqueue(value: i)
         }
 
         for _ in 0 ..< k {
             var v = h.dequeue()!
             total -= v / 2
             if v % 2 == 0 {
-                h.enqueue(v / 2)
+                h.enqueue(value: v / 2)
             } else {
-                h.enqueue((v / 2) + 1)
+                h.enqueue(value: (v / 2) + 1)
             }
         }
 
         return total
     }
 
-    public struct PriorityQueue<T> {
-        fileprivate var heap: Heap<T>
+    struct Heap<T> where T: Equatable {
+        var list = [T]()
+        var comparison: (T, T) -> Bool
 
-        /*
-           To create a max-priority queue, supply a > sort function. For a min-priority
-           queue, use <.
-         */
-        public init(sort: @escaping (T, T) -> Bool) {
-            heap = Heap(sort: sort)
+        // find index
+        func leftChild(of index: Int) -> Int {
+            2 * index + 1
         }
 
-        public var isEmpty: Bool {
-            heap.isEmpty
+        func rightChild(of index: Int) -> Int {
+            2 * index + 2
         }
 
-        public var count: Int {
-            heap.count
+        func parentIndex(of index: Int) -> Int {
+            // actually, (index-1)/2 is for odd, (index-2)/2 is for even, but they are same result
+            (index - 1) / 2
         }
 
-        public func peek() -> T? {
-            heap.peek()
+        // compare
+        func isHigherPriority(at firstIndex: Int, than secondIndex: Int) -> Bool {
+            comparison(list[firstIndex], list[secondIndex])
         }
 
-        public mutating func enqueue(_ element: T) {
-            heap.insert(element)
+        func childHigherPriority(parent index: Int) -> Int? {
+            let left = leftChild(of: index)
+            let right = rightChild(of: index)
+            let total = list.count
+
+            if left < total, right < total {
+                if isHigherPriority(at: left, than: index) || isHigherPriority(at: right, than: index) {
+                    return isHigherPriority(at: left, than: right) ? left : right
+                } else {
+                    return nil
+                }
+            } else if left < total {
+                return isHigherPriority(at: left, than: index) ? left : nil
+            } else if right < total {
+                return isHigherPriority(at: right, than: index) ? right : nil
+            }
+            return nil
         }
 
-        public mutating func dequeue() -> T? {
-            heap.remove()
-        }
-
-        /*
-           Allows you to change the priority of an element. In a max-priority queue,
-           the new priority should be larger than the old one; in a min-priority queue
-           it should be smaller.
-         */
-        public mutating func changePriority(index i: Int, value: T) {
-            heap.replace(index: i, value: value)
-        }
-    }
-
-    public struct Heap<T> {
-        /** The array that stores the heap's nodes. */
-        var nodes = [T]()
-
-        /**
-         * Determines how to compare two nodes in the heap.
-         * Use '>' for a max-heap or '<' for a min-heap,
-         * or provide a comparing method if the heap is made
-         * of custom elements, for example tuples.
-         */
-        private var orderCriteria: (T, T) -> Bool
-
-        /**
-         * Creates an empty heap.
-         * The sort function determines whether this is a min-heap or max-heap.
-         * For comparable data types, > makes a max-heap, < makes a min-heap.
-         */
-        public init(sort: @escaping (T, T) -> Bool) {
-            orderCriteria = sort
-        }
-
-        /**
-         * Creates a heap from an array. The order of the array does not matter;
-         * the elements are inserted into the heap in the order determined by the
-         * sort function. For comparable data types, '>' makes a max-heap,
-         * '<' makes a min-heap.
-         */
-        public init(array: [T], sort: @escaping (T, T) -> Bool) {
-            orderCriteria = sort
-            configureHeap(from: array)
-        }
-
-        /**
-         * Configures the max-heap or min-heap from an array, in a bottom-up manner.
-         * Performance: This runs pretty much in O(n).
-         */
-        private mutating func configureHeap(from array: [T]) {
-            nodes = array
-            for i in stride(from: nodes.count / 2 - 1, through: 0, by: -1) {
-                shiftDown(i)
+        // sift: up(enqueue) and down(dequeue)
+        private mutating func siftUp(index: Int) {
+            let parentIndex = parentIndex(of: index)
+            if isHigherPriority(at: index, than: parentIndex) {
+                list.swapAt(index, parentIndex)
+                siftUp(index: parentIndex)
             }
         }
 
-        public var isEmpty: Bool {
-            nodes.isEmpty
-        }
-
-        public var count: Int {
-            nodes.count
-        }
-
-        /**
-         * Returns the index of the parent of the element at index i.
-         * The element at index 0 is the root of the tree and has no parent.
-         */
-        @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
-            (i - 1) / 2
-        }
-
-        /**
-         * Returns the index of the left child of the element at index i.
-         * Note that this index can be greater than the heap size, in which case
-         * there is no left child.
-         */
-        @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
-            2 * i + 1
-        }
-
-        /**
-         * Returns the index of the right child of the element at index i.
-         * Note that this index can be greater than the heap size, in which case
-         * there is no right child.
-         */
-        @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
-            2 * i + 2
-        }
-
-        /**
-         * Returns the maximum value in the heap (for a max-heap) or the minimum
-         * value (for a min-heap).
-         */
-        public func peek() -> T? {
-            nodes.first
-        }
-
-        /**
-         * Adds a new value to the heap. This reorders the heap so that the max-heap
-         * or min-heap property still holds. Performance: O(log n).
-         */
-        public mutating func insert(_ value: T) {
-            nodes.append(value)
-            shiftUp(nodes.count - 1)
-        }
-
-        /**
-         * Adds a sequence of values to the heap. This reorders the heap so that
-         * the max-heap or min-heap property still holds. Performance: O(log n).
-         */
-        public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
-            for value in sequence {
-                insert(value)
+        private mutating func siftDown(index: Int) {
+            if let child = childHigherPriority(parent: index) {
+                list.swapAt(child, index)
+                siftDown(index: child)
             }
         }
 
-        /**
-         * Allows you to change an element. This reorders the heap so that
-         * the max-heap or min-heap property still holds.
-         */
-        public mutating func replace(index i: Int, value: T) {
-            guard i < nodes.count else { return }
-
-            remove(at: i)
-            insert(value)
+        // get data
+        mutating func enqueue(value: T) {
+            list.append(value)
+            siftUp(index: list.count - 1)
         }
 
-        /**
-         * Removes the root node from the heap. For a max-heap, this is the maximum
-         * value; for a min-heap it is the minimum value. Performance: O(log n).
-         */
-        @discardableResult public mutating func remove() -> T? {
-            guard !nodes.isEmpty else { return nil }
-
-            if nodes.count == 1 {
-                return nodes.removeLast()
-            } else {
-                // Use the last node to replace the first one, then fix the heap by
-                // shifting this new first node into its proper position.
-                let value = nodes[0]
-                nodes[0] = nodes.removeLast()
-                shiftDown(0)
-                return value
+        mutating func dequeue() -> T? {
+            if list.isEmpty {
+                return nil
             }
-        }
-
-        /**
-         * Removes an arbitrary node from the heap. Performance: O(log n).
-         * Note that you need to know the node's index.
-         */
-        @discardableResult public mutating func remove(at index: Int) -> T? {
-            guard index < nodes.count else { return nil }
-
-            let size = nodes.count - 1
-            if index != size {
-                nodes.swapAt(index, size)
-                shiftDown(from: index, until: size)
-                shiftUp(index)
-            }
-            return nodes.removeLast()
-        }
-
-        /**
-         * Takes a child node and looks at its parents; if a parent is not larger
-         * (max-heap) or not smaller (min-heap) than the child, we exchange them.
-         */
-        internal mutating func shiftUp(_ index: Int) {
-            var childIndex = index
-            let child = nodes[childIndex]
-            var parentIndex = parentIndex(ofIndex: childIndex)
-
-            while childIndex > 0, orderCriteria(child, nodes[parentIndex]) {
-                nodes[childIndex] = nodes[parentIndex]
-                childIndex = parentIndex
-                parentIndex = self.parentIndex(ofIndex: childIndex)
-            }
-
-            nodes[childIndex] = child
-        }
-
-        /**
-         * Looks at a parent node and makes sure it is still larger (max-heap) or
-         * smaller (min-heap) than its childeren.
-         */
-        internal mutating func shiftDown(from index: Int, until endIndex: Int) {
-            let leftChildIndex = leftChildIndex(ofIndex: index)
-            let rightChildIndex = leftChildIndex + 1
-
-            // Figure out which comes first if we order them by the sort function:
-            // the parent, the left child, or the right child. If the parent comes
-            // first, we're done. If not, that element is out-of-place and we make
-            // it "float down" the tree until the heap property is restored.
-            var first = index
-            if leftChildIndex < endIndex, orderCriteria(nodes[leftChildIndex], nodes[first]) {
-                first = leftChildIndex
-            }
-            if rightChildIndex < endIndex, orderCriteria(nodes[rightChildIndex], nodes[first]) {
-                first = rightChildIndex
-            }
-            if first == index { return }
-
-            nodes.swapAt(index, first)
-            shiftDown(from: first, until: endIndex)
-        }
-
-        internal mutating func shiftDown(_ index: Int) {
-            shiftDown(from: index, until: nodes.count)
+            let tmp = list[0]
+            list.swapAt(list.count - 1, 0)
+            list.removeLast()
+            siftDown(index: 0)
+            return tmp
         }
     }
 }
