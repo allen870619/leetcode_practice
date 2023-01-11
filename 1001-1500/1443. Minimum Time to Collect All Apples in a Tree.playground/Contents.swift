2b4efac858ad8da69f7201851a8d5d958f1b6993
Allen Lee <allen870619@gmail.com>
2023-01-11 09:09:51 +0800

    Adding BFS solution for No.1443

--- 1001-1500/1443. Minimum Time to Collect All Apples in a Tree.playground/Contents.swift
+++ 1001-1500/1443. Minimum Time to Collect All Apples in a Tree.playground/Contents.swift
@@ -38,6 +38,49 @@ class Solution {
     }
 }
 
+/// Bottom-up BFS
+class Solution2 {
+    func minTime(_ n: Int, _ edges: [[Int]], _ hasApple: [Bool]) -> Int {
+        // create node
+        var dictNode = [Int: Int]()
+        for i in edges {
+            if dictNode[i[1]] != nil {
+                dictNode[i[0]] = i[1]
+            } else {
+                dictNode[i[1]] = i[0]
+            }
+        }
+
+        // create bfs
+        var hasApple = hasApple
+        var list = [Int]()
+        var ran = Array(repeating: false, count: n)
+        for index in 0 ..< n {
+            if hasApple[index] {
+                list.append(index)
+            }
+        }
+        while !list.isEmpty {
+            for _ in 0 ..< list.count {
+                let node = list.removeFirst()
+                hasApple[node] = true
+                ran[node] = true
+                if let parent = dictNode[node], !ran[parent] {
+                    list.append(parent)
+                }
+            }
+        }
+
+        // calculate tree height
+        let avbl = hasApple.filter { $0 == true }.count
+        if avbl == 0 {
+            return 0
+        } else {
+            return (avbl - 1) * 2
+        }
+    }
+}
+
 let sol = Solution()
 print(sol.minTime(7, [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]], [false, false, true, false, true, true, false]))
 
