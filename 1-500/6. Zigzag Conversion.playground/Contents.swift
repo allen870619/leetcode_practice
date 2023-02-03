f0d4db9cbbf9b1b9c33b546f420a4e0480de738a
Allen Lee <allen870619@gmail.com>
2023-02-03 10:15:22 +0800

    6. Zigzag Conversion (Second)

--- 1-500/6. Zigzag Conversion.playground/Contents.swift
+++ 1-500/6. Zigzag Conversion.playground/Contents.swift
@@ -1,3 +1,44 @@
+// 20230203
+class Solution3 {
+    func convert(_ s: String, _ numRows: Int) -> String {
+        if numRows == 1 {
+            return s
+        }
+        var skip = 0, space = numRows
+        var result = ""
+        let s = [Character](s)
+
+        for i in 0 ..< numRows {
+            var index = i
+            while index < s.count {
+                result += "\(s[index])"
+
+                let spaceSize = (space - 1) * 2 - 1 < 0 ? 0 : (space - 1) * 2 - 1
+                if i != numRows - 1 {
+                    index += spaceSize + 1
+                }
+
+                if index >= s.count {
+                    break
+                }
+
+                if i != 0, i != numRows - 1 {
+                    result += "\(s[index])"
+                }
+
+                let skipSize = skip * 2 - 1 < 0 ? 0 : skip * 2 - 1
+                if i != 0 {
+                    index += skipSize + 1
+                }
+            }
+            skip += 1
+            space -= 1
+        }
+        return result
+    }
+}
+
+// 20211210
 class Solution {
     func convert(_ s: String, _ numRows: Int) -> String {
         if numRows == 1 {
