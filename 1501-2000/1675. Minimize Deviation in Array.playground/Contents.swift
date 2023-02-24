8799027a0e602a75409ed6ffe95740955bca46c9
Allen Lee <allen870619@gmail.com>
2023-02-24 12:29:50 +0800

    1675. Minimize Deviation in Array

--- 1501-2000/1675. Minimize Deviation in Array.playground/Contents.swift
+++ 1501-2000/1675. Minimize Deviation in Array.playground/Contents.swift
@@ -1,3 +1,113 @@
+// 20230224
+class Solution2 {
+    func minimumDeviation(_ nums: [Int]) -> Int {
+        var pq = PriorityQueue<Int>(comparison: >)
+        var mini = Int.max
+        for i in nums {
+            mini = min(mini, i % 2 == 0 ? i : i * 2)
+            pq.enqueue(value: i % 2 == 0 ? i : i * 2)
+        }
+
+        var result = Int.max
+        while (pq.peek() ?? 1) % 2 == 0 {
+            let v = pq.dequeue()! / 2
+            pq.enqueue(value: v)
+            if v < mini {
+                mini = v
+            }
+            result = min(result, (pq.peek() ?? Int.max) - mini)
+        }
+        return result
+    }
+}
+
+struct PriorityQueue<T> where T: Equatable {
+    var list = [T]()
+    var comparison: (T, T) -> Bool
+
+    // find index
+    private func leftChild(of index: Int) -> Int {
+        2 * index + 1
+    }
+
+    private func rightChild(of index: Int) -> Int {
+        2 * index + 2
+    }
+
+    private func parentIndex(of index: Int) -> Int {
+        // actually, (index-1)/2 is for odd, (index-2)/2 is for even, but they are same result
+        (index - 1) / 2
+    }
+
+    // compare
+    private func isHigherPriority(at firstIndex: Int, than secondIndex: Int) -> Bool {
+        comparison(list[firstIndex], list[secondIndex])
+    }
+
+    /// compare parent, left child and right child.
+    ///
+    /// If parent's priority is highest, if will return `nil`, otherwise return higher index
+    private func childHigherPriority(parent index: Int) -> Int? {
+        let left = leftChild(of: index)
+        let right = rightChild(of: index)
+        let total = list.count
+
+        if left < total, right < total {
+            if isHigherPriority(at: left, than: index) || isHigherPriority(at: right, than: index) {
+                return isHigherPriority(at: left, than: right) ? left : right
+            } else {
+                return nil
+            }
+        } else if left < total {
+            return isHigherPriority(at: left, than: index) ? left : nil
+        } else if right < total {
+            return isHigherPriority(at: right, than: index) ? right : nil
+        }
+        return nil
+    }
+
+    // sift: up(enqueue) and down(dequeue)
+    // enqueue
+    private mutating func siftUp(index: Int) {
+        let parentIndex = parentIndex(of: index)
+        if isHigherPriority(at: index, than: parentIndex) {
+            list.swapAt(index, parentIndex)
+            siftUp(index: parentIndex)
+        }
+    }
+
+    // dequeue
+    private mutating func siftDown(index: Int) {
+        if let child = childHigherPriority(parent: index) {
+            list.swapAt(child, index)
+            siftDown(index: child)
+        }
+    }
+
+    // get data
+    mutating func enqueue(value: T) {
+        list.append(value)
+        siftUp(index: list.count - 1)
+    }
+
+    mutating func dequeue() -> T? {
+        if list.isEmpty {
+            return nil
+        }
+
+        let tmp = list[0]
+        list.swapAt(list.count - 1, 0)
+        list.removeLast()
+        siftDown(index: 0)
+        return tmp
+    }
+
+    func peek() -> T? {
+        list.first
+    }
+}
+
+// 20220219
 class Solution {
     func minimumDeviation(_ nums: [Int]) -> Int {
         var set = [Int]()
