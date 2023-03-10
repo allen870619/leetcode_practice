547f499b520bbb9f2b4b8b5da04e2ba92621d0fd
Allen Lee <allen870619@gmail.com>
2023-03-10 09:32:54 +0800

    382. Linked List Random Node

--- 1-500/382. Linked List Random Node.playground/Contents.swift
+++ 1-500/382. Linked List Random Node.playground/Contents.swift
@@ -6,6 +6,51 @@ public class ListNode {
     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
 }
 
+// Time complexity: O(n) (create), random: O(n)
+// space: O(1)
+class Solution4 {
+    var head: ListNode?
+    var total = 0
+    init(_ head: ListNode?) {
+        self.head = head
+        var tmp = head
+
+        while tmp != nil {
+            total += 1
+            tmp = tmp?.next
+        }
+    }
+
+    func getRandom() -> Int {
+        let index = Int.random(in: 0 ..< total)
+        var tmp = head
+        for _ in 0 ..< index {
+            tmp = tmp?.next
+        }
+        return tmp?.val ?? 0
+    }
+}
+
+// Time complexity: O(n) (create)
+// space: O(n)
+class Solution3 {
+    var list = [Int]()
+    init(_ head: ListNode?) {
+        var head = head
+        while head != nil {
+            if let tmp = head {
+                list.append(tmp.val)
+            }
+            head = head?.next
+        }
+    }
+
+    func getRandom() -> Int {
+        let index = Int.random(in: 0 ..< list.count)
+        return list[index]
+    }
+}
+
 class Solution {
     var head: ListNode?
     init(_ head: ListNode?) {
@@ -49,7 +94,7 @@ class Solution2 {
         }
 
         let rand = Int.random(in: 0 ..< nodeList.count)
-        return nodeList[rand].val ?? -1
+        return nodeList[rand]?.val ?? -1
     }
 }
 
